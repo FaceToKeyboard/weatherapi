b46b3c7e2c7c5a87f8efbba62a54322de6f81f4c
Fanno Chea <fanno.chea@gmail.com>
2022-01-28 11:57:16 -0700

    test for when upstream API contains full response

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const axios = require('axios');
 
 const server = require('../server/server.js');
-const { fullResponse } = require('./apidummydata.js');
+const { fullResponse, missingWeather } = require('./apidummydata.js');
 
 jest.mock('axios');
 
@@ -11,8 +11,7 @@ describe('API response correctness', () => {
     data: fullResponse,
   })
 
-  test('Response contains all expected keys', async (done) => {
-
+  test('Response contains all expected keys', async () => {
     const response = await request(server).get('/local/80113');
 
     expect(response.status).toBe(200);
@@ -20,9 +19,7 @@ describe('API response correctness', () => {
     expect(response.body).toHaveProperty('conditions');
     expect(response.body).toHaveProperty('high_temp');
     expect(response.body).toHaveProperty('low_temp');
-
   });
-
 });
 
 describe.skip('Upstream API error handling', () => {
