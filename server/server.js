002222698fa1429c209d32a7b4704d94e3d6b178
Fanno Chea <fanno.chea@gmail.com>
2022-01-28 13:50:32 -0700

    simple zipcode format check before API call

--- server/server.js
+++ server/server.js
@@ -8,18 +8,22 @@ const server = express();
 server.use(express.json());
 
 server.get('/local/:zipcode', async (req, res) => {
+  const zipcode = req.params.zipcode;
+  const re = /\D/;
+  if (re.exec(zipcode) !== null || zipcode.length !== 5) return res.status(404).send('city not found');
+
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${req.params.zipcode}&appid=${process.env.API_KEY}`;
     const apiresponse = await axios.get(url);
 
-    if (apiresponse.data) {
+    if (apiresponse.status === 200) {
       const data = {
-        city: apiresponse.data.name,
-        conditions: apiresponse.data.weather[0].description,
-        high_temp: Math.ceil(apiresponse.data.main.temp_max - 273),
-        low_temp: Math.floor(apiresponse.data.main.temp_min - 273),
+        city: apiresponse.data.name ?? 'No data',
+        conditions: apiresponse.data.weather?.[0].description ?? 'No data',
+        high_temp: apiresponse.data.main?.temp_max ? Math.ceil(apiresponse.data.main.temp_max - 273) : 'No data',
+        low_temp: apiresponse.data.main?.temp_max ? Math.floor(apiresponse.data.main.temp_min - 273) : 'No data',
       }
-      res.send(data);
+      res.status(200).send(data);
     } else {
       res.status(502).send('Bad Gateway');
       console.log('API did not respond with data')
