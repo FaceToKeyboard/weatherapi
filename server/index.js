1d41c9d788aced5333f9d42ad224865a85b32c14
Fanno Chea <fanno.chea@gmail.com>
2022-01-28 15:10:44 -0700

    server port defaults to 3000

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 server.listen(port, () => {
   console.log(`${new Date().toLocaleString()}: Server started on port ${port}`);
