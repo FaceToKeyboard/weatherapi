00afab846869d57d6755b5fa00749f40a499fd1e
Fanno Chea <fanno.chea@gmail.com>
2022-01-28 11:27:32 -0700

    server start timestamp

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ import { server } from './server.js';
 const port = process.env.SERVER_PORT;
 
 server.listen(port, () => {
-  console.log('Server started on port ', port);
+  console.log(`${new Date().toLocaleString()}: Server started on port ${port}`);
 })
