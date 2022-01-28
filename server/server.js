d1d8e1eeec797f0b48583c4383fbb057f3b812c7
Fanno Chea <fanno.chea@gmail.com>
2022-01-28 14:43:59 -0700

    better API request error handling

--- server/server.js
+++ server/server.js
@@ -7,33 +7,53 @@ const weatherApi = {
 const server = express();
 server.use(express.json());
 
+const apiRequest =  axios.create({
+
+});
+
 server.get('/local/:zipcode', async (req, res) => {
   const zipcode = req.params.zipcode;
   const re = /\D/;
-  if (re.exec(zipcode) !== null || zipcode.length !== 5) return res.status(404).send('city not found');
+  if (re.exec(zipcode) !== null || zipcode.length !== 5) return res.status(404).send('Bad Request');
 
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${req.params.zipcode}&appid=${process.env.API_KEY}`;
     const apiresponse = await axios.get(url);
 
-    if (apiresponse.status === 200) {
-      const data = {
-        city: apiresponse.data.name ?? 'No data',
-        conditions: apiresponse.data.weather?.[0].description ?? 'No data',
-        high_temp: apiresponse.data.main?.temp_max ? Math.ceil(apiresponse.data.main.temp_max - 273) : 'No data',
-        low_temp: apiresponse.data.main?.temp_max ? Math.floor(apiresponse.data.main.temp_min - 273) : 'No data',
-      }
-      res.status(200).send(data);
+    const data = {
+      city: apiresponse.data.name ?? 'No data',
+      conditions: apiresponse.data.weather?.[0].description ?? 'No data',
+      high_temp: apiresponse.data.main?.temp_max ? Math.ceil(apiresponse.data.main.temp_max - 273) : 'No data',
+      low_temp: apiresponse.data.main?.temp_max ? Math.floor(apiresponse.data.main.temp_min - 273) : 'No data',
+    }
+    return res.status(200).send(data);
+  } catch(e) {
+    // axios will throw an error if the server responded with a status
+    // code that falls out of 2xx range,
+    // as well as any other general error
+    if (e.response) {
+      // upstream responded with a status code
+      // out of 2xx range
+      console.log(e.response.data);
+      console.log(e.response.status);
+      console.log(e.response.headers);
+      return res.status(e.response.status).send(e.response.data);
+    } else if (e.request) {
+      // The request was made but no response was received
+      console.log(e.request);
+      return res.status(504).send('API request timed out. Try again later.');
     } else {
-      res.status(502).send('Bad Gateway');
-      console.log('API did not respond with data')
+      // Something happened in setting up the request that triggered an error
+      console.log(`Error making API request: \n${e}`);
+      return res.status(500).send('Internal server error.');
     }
 
-  } catch(e) {
-    console.log(`Error making API request: \n${e}`);
-    res.status(502).send('Bad Gateway');
   }
+}); // end server.get()
 
+// all other routes return 404
+server.use((req, res) => {
+  res.status(404).send('Not found');
 });
 
 module.exports = server;
